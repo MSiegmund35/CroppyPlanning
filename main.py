b47e9c1707ec7df7c6ebb8abb27b89868564684d
Michael S Siegmund <mssiegmund@gmail.com>
2022-02-25 22:08:23 -0500

    Initial commit of main.py!

---
+++ main.py
@@ -0,0 +1,9 @@
+# CroppyPlanning
+# Michael Siegmund
+# V0.0 25 Feb 2022
+# WE ARE!
+
+# This program is designed to facilitate planting, seeding, and harvesting planning
+#     for a small scale vegitable farm. I hope it helps!
+
+print('mmmmmmmmmmmmmm doughnuts.. I mean veggies........')
